67fb257be4f205369ee35419388132c61731d715
Filip Stankovic <filips@gmail.com>
2016-01-18 14:04:09 -0500

    All calculator tests pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -16,4 +16,10 @@ def multiply(*num)
 end
 
 def factorial(num)
+  if num == 0 then return 1 end
+  num_array = (1..num).to_a
+  num_array.inject(:*)
+  # recursion method:
+  # return 1 if num == 0
+  # num * factorial(num - 1)
 end
